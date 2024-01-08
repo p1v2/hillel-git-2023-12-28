0da4902ca3943b1db60da89236997c7a8c0f2a36
p1v2 <pavliuk96@gmail.com>
2024-01-08 20:05:13 +0200

    add windows 10 ad

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ def start_windows():
     print("Hello from Windows 10!")
     print("I am a Windows 10 user.")
 
+    print("Widows 10 is the best OS ever!")
+
 
 if __name__ == "__main__":
     start_windows()
